c1e4dbb6a5a7c7f2ba6b696f07bde97ada439230
Nur <furiousnur@gmail.com>
2023-08-15 22:52:19 +0600

    blogData.json file created and used

--- app/blog/[id]/page.js
+++ app/blog/[id]/page.js
@@ -3,11 +3,11 @@ import "./style.css";
 
 export default async function SingleBlog({ params }) {
     const id = params.id;
-    const post = await getSinglePost(id);
-
+    const post = await getSinglePost(id); 
     return (
         <div className="single-post">
-            <h1 className="single-title">{post.title}</h1>
+            <h5 className="single-title">{post.title}</h5>
+            <p className="date">{post.date}</p>
             <p className="single-content">{post.body}</p>
             <a href="/blog" className="back-link">Back to Blog</a>
         </div>
